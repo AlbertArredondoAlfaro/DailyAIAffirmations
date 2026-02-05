570f5c1235b7adf7dfcf971b43047769d47eb069
Albert Arredondo <albertarredondoalfaro@gmail.com>
2026-02-05 09:13:57 +0100

    Initial Commit

---
+++ Daily AI Affirmations/Daily_AI_AffirmationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_AI_AffirmationsApp.swift
+//  Daily AI Affirmations
+//
+//  Created by Albert Bit Dj on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Daily_AI_AffirmationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
